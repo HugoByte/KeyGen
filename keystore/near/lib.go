289e904e514f4945258c2a84d934fdd338d4a5db
s0u1z <shreyas.ks@hugobyte.com>
2021-11-17 22:39:03 +0530

    Fix EncryptKey

--- keystore/near/lib.go
+++ keystore/near/lib.go
@@ -35,79 +35,81 @@ func sha3sumkeccak256(data ...[]byte) []byte {
 
 func EncryptKey(kP interface{}, pw string, file string) error {
 
-	keypair, Ok := (kP).(*Keypair)
-	if !Ok {
-		return fmt.Errorf("some error")
+	if keypair, Ok := (kP).(*Keypair); Ok {
+		s := keypair.privateKey
+		var ks common.KeyStoreData
+		var k common.ScryptParams
+		var c common.AES128CTRParams
+
+		///password to bytes of size 64
+		passBytes := sha512.Sum512([]byte(pw))
+
+		if err := k.Init(s); err != nil {
+			return err
+		}
+
+		key, err := k.Key(passBytes[:])
+		if err != nil {
+			return err
+		}
+
+		ks.Crypto.KDF = kdfScrypt
+		ks.Crypto.KDFParams, err = json.Marshal(&k)
+		if err != nil {
+			return err
+		}
+
+		cipherBlock, err := aes.NewCipher(key[0:16])
+		if err != nil {
+			return err
+		}
+
+		//Initialization Vector
+		c.IV = make([]byte, cipherBlock.BlockSize())
+		_, err = io.ReadFull(rand.Reader, c.IV)
+		if err != nil {
+			return err
+		}
+
+		//creating cipher text
+
+		cipherText := make([]byte, len(s))
+		//create cipher stream
+		cipherStrem := cipher.NewCTR(cipherBlock, c.IV)
+
+		//creating encrypted key
+
+		cipherStrem.XORKeyStream(cipherText, s)
+
+		ks.Crypto.Cipher = cipherAES128CTR
+		ks.Crypto.CipherParams, err = json.Marshal(&c)
+		if err != nil {
+			return err
+		}
+
+		ks.Crypto.CipherText = cipherText
+
+		ks.Crypto.MAC = sha3sumkeccak256(key[16:32], cipherText)
+		ks.CoinType = coin
+		ks.Version = 3
+		ks.ID = uuid.Must(uuid.NewV4()).String()
+
+		address := hex.EncodeToString(s.Public().(ed25519.PublicKey))
+
+		ks.Address = []byte(address)
+
+		output, err := json.Marshal(&ks)
+
+		if err != nil {
+			return err
+		}
+
+		return common.WriteFile(file, output, 0600)
+
 	}
-	s := keypair.privateKey
-	var ks common.KeyStoreData
-	var k common.ScryptParams
-	var c common.AES128CTRParams
 
-	///password to bytes of size 64
-	passBytes := sha512.Sum512([]byte(pw))
+	return fmt.Errorf("failed to cast keypair")
 
-	if err := k.Init(s); err != nil {
-		return err
-	}
-
-	key, err := k.Key(passBytes[:])
-	if err != nil {
-		return err
-	}
-
-	ks.Crypto.KDF = kdfScrypt
-	ks.Crypto.KDFParams, err = json.Marshal(&k)
-	if err != nil {
-		return err
-	}
-
-	cipherBlock, err := aes.NewCipher(key[0:16])
-	if err != nil {
-		return err
-	}
-
-	//Initialization Vector
-	c.IV = make([]byte, cipherBlock.BlockSize())
-	_, err = io.ReadFull(rand.Reader, c.IV)
-	if err != nil {
-		return err
-	}
-
-	//creating cipher text
-
-	cipherText := make([]byte, len(s))
-	//create cipher stream
-	cipherStrem := cipher.NewCTR(cipherBlock, c.IV)
-
-	//creating encrypted key
-
-	cipherStrem.XORKeyStream(cipherText, s)
-
-	ks.Crypto.Cipher = cipherAES128CTR
-	ks.Crypto.CipherParams, err = json.Marshal(&c)
-	if err != nil {
-		return err
-	}
-
-	ks.Crypto.CipherText = cipherText
-
-	ks.Crypto.MAC = sha3sumkeccak256(key[16:32], cipherText)
-	ks.CoinType = coin
-	ks.Version = 3
-	ks.ID = uuid.Must(uuid.NewV4()).String()
-
-	address := hex.EncodeToString(s.Public().(ed25519.PublicKey))
-
-	ks.Address = []byte(address)
-
-	output, err := json.Marshal(&ks)
-
-	if err != nil {
-		return err
-	}
-
-	return common.WriteFile(file, output, 0600)
 }
 
 func DecryptKey(ksData *common.KeyStoreData, password string) (ed25519.PrivateKey, error) {
